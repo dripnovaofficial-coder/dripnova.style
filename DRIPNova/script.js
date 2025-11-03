16eb3fc766e7deea2d76540ebdb9c34d905d5e3d
dripnovaofficial-coder <dripnova.official@gmail.com>
2025-11-04 01:07:40 +0500

    Add files via upload

---
+++ DRIPNova/script.js
@@ -0,0 +1,7 @@
+function buyNow(item, price) {
+  alert(
+    `To purchase ${item} for PKR ${price}, please pay via:\n\n` +
+    `💸 JazzCash / Easypaisa: 0330-2936798\n🏦 Bank Transfer: DripNova Official Account\n📱 NayaPay: dripnova@nayapay.com\n\n` +
+    `After payment, send screenshot to Instagram @dripnova.official`
+  );
+}
