7ff6053ee6d23689401f66c86bc2ecbe8542ae4e
dripnovaofficial-coder <dripnova.official@gmail.com>
2026-03-24 19:55:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,23 @@ let container = document.getElementById("products");
 
 if (container) {
   fetch('products.json')
-    .then(response => response.json())
+    .then(res => res.json())
     .then(data => {
+
       let html = "";
 
       data.forEach(product => {
 
-        // Get first color
-        let firstColor = Object.keys(product.colors)[0];
+        // Get first color (Black, White etc)
+        let colors = product.colors;
+        let firstColor = Object.keys(colors)[0];
 
         // Get first image of that color
-        let image = product.colors[firstColor][0];
+        let image = colors[firstColor][0];
 
         html += `
           <div class="product">
-            <img src="${image}">
+            <img src="${image}" alt="${product.name}">
             <h3>${product.name}</h3>
             <p>Rs ${product.price}</p>
           </div>
@@ -24,9 +26,10 @@ if (container) {
       });
 
       container.innerHTML = html;
+
     })
-    .catch(error => {
-      console.error(error);
-      container.innerHTML = "Failed to load products";
+    .catch(err => {
+      console.error(err);
+      container.innerHTML = "Error loading products";
     });
 }
